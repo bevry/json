a7a4923efd374026303e133ce47ef3e55085b341
Benjamin Lupton <b@lupton.cc>
2023-11-23 11:56:02 +0800

    v2.0.0 - boundation, changelog, version bump

--- source/test.ts
+++ source/test.ts
@@ -1,7 +1,13 @@
+// external
 import { deepEqual } from 'assert-helpers'
 import kava from 'kava'
+import promiseErrback from 'promise-errback'
+
+// builtin
 import { tmpdir } from 'os'
 import { join } from 'path'
+
+// local
 import { json, readJSON, writeJSON, deleteJSON } from './index.js'
 
 kava.suite('@bevry/json', function (suite, test) {
@@ -28,15 +34,16 @@ kava.suite('@bevry/json', function (suite, test) {
 		deepEqual(actual, expected, 'was as expected')
 	})
 	test('fs works as expected', function (done) {
-		Promise.resolve()
-			.then(async function () {
+		promiseErrback(
+			Promise.resolve().then(async function () {
 				const tmp = join(tmpdir(), `bevry-json-${Math.random()}.txt`)
 				await deleteJSON(tmp) // ensure it does not exist, should not fail if it does not exist
 				const data = { a: 1 }
 				await writeJSON(tmp, data)
 				deepEqual(await readJSON(tmp), data, 'has the data we expected')
 				await deleteJSON(tmp)
-			})
-			.finally(done)
+			}),
+			done
+		)
 	})
 })
